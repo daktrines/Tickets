b964626d342f5678fa23f478ae99f7c19bd7750d
Hapro Bishop <acfowo45@gmail.com>
2022-11-09 22:58:18 +0300

    Пофикшено

--- Tickets/Flight/AddForm5.xaml.cs
+++ Tickets/Flight/AddForm5.xaml.cs
@@ -65,15 +65,15 @@ namespace Tickets
                 else p1.КодРейса = RaceId;
                 #endregion
                 p1.КодАвиакомпании = db.Авиакомпании.Local.ToBindingList().Where(p => p.Наименование == Наименование.Text).First().КодАвиакомпании;
-                p1.КодСамолёта = db.Самолеты.Local.ToBindingList().Where(p => p.МодельСамолета == ((Самолеты)МодельСамолета.SelectedValue).МодельСамолета).КодСамолета;
+                p1.КодСамолёта = db.Самолеты.Local.ToBindingList().Where(p => p.МодельСамолета == ((Самолеты)МодельСамолета.SelectedValue).МодельСамолета).First().КодСамолета;
                 p1.ДатаОтправления = (DateTime)ДатаОтправления.SelectedDate;
                 p1.ДатаПрибытия = (DateTime)ДатаПрибытия.SelectedDate;
                 p1.ВремяОтправления = new TimeSpan(Convert.ToInt32(Часы.Text), Convert.ToInt32(Минуты.Text), 0);
                 p1.ВремяПрибытия = new TimeSpan(Convert.ToInt32(Часы1.Text), Convert.ToInt32(Минуты1.Text), 0);
                 p1.ДлительностьПерелета = ДлительностьПерелета.Text; 
                 p1.СтоимостьБилета = Convert.ToDecimal(СтоимостьБилета.Text);
-                p1.Отправление = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_отправление.SelectedValue).Город);
-                p1.Прибытие = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_назначение.SelectedValue).Город);
+                p1.Отправление = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_отправление.SelectedValue).Город).First().КодАэропорта;
+                p1.Прибытие = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_назначение.SelectedValue).Город).First().КодАэропорта;
                 #endregion
                 db.Рейсы.Add(p1);
                 //Сохраняем изменения
