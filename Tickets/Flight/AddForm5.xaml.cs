99798995f0e97455b7e6c62009089dece818dfd0
Hapro Bishop <acfowo45@gmail.com>
2022-11-09 22:41:27 +0300

    Финальная редакция добавления

--- Tickets/Flight/AddForm5.xaml.cs
+++ Tickets/Flight/AddForm5.xaml.cs
@@ -31,68 +31,59 @@ namespace Tickets
         private void AddMain_Click(object sender, RoutedEventArgs e)
         {
             ////Проверка каждого обязательного для заполнения поля
-            //StringBuilder errors = new StringBuilder();
-            //if (КодБилета.Text.Length == 0 || double.TryParse(КодБилета.Text, out double x1) == false || x1 < 1) errors.AppendLine("Введите код билета");
-            //if (КодРейса.Text.Length == 0 || double.TryParse(КодРейса.Text, out double x2) == false || x2 < 1) errors.AppendLine("Введите код рейса");
-            //if (Наименование.Text.Length == 0) errors.AppendLine("Введите наименование авиакомпании");
-            //if (Фамилия.Text.Length == 0) errors.AppendLine("Введите фамилию");
-            //if (НазваниеКласса.Text.Length == 0) errors.AppendLine("Введите название класса");
-            //if (Багаж.Text.Length == 0) errors.AppendLine("Введите багаж");
+            StringBuilder errors = new StringBuilder();
+            if (КодРейса.Text.Length == 0 || double.TryParse(КодРейса.Text, out double x2) == false || x2 < 1) errors.AppendLine("Введите код рейса");
+            if (Наименование.Text.Length == 0) errors.AppendLine("Введите наименование авиакомпании");
+            if (МодельСамолета.Text.Length == 0) errors.AppendLine("Введите модель самолета");
+            if (ДатаОтправления.Text.Length == 0) errors.AppendLine("Введите дату отправления");
+            if (ДатаПрибытия.Text.Length == 0) errors.AppendLine("Введите дату прибытия");
+            if (Часы.Text.Length == 0) errors.AppendLine("Введите время отправления");
+            if (Минуты.Text.Length == 0) errors.AppendLine("Введите время прибытия");
+            if (Часы1.Text.Length == 0) errors.AppendLine("Введите время отправления");
+            if (Минуты1.Text.Length == 0) errors.AppendLine("Введите время прибытия");
+            if (ДлительностьПерелета.Text.Length == 0) errors.AppendLine("Введите длительность перелета");
+            if (СтоимостьБилета.Text.Length == 0) errors.AppendLine("Введите стоимость билета");
+            if (Город_отправление.Text.Length == 0) errors.AppendLine("Введите дату отправления");
+            if (Город_назначение.Text.Length == 0) errors.AppendLine("Введите дату назначение");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
 
-            //if (errors.Length > 0)
-            //{
-            //    MessageBox.Show(errors.ToString());
-            //    return;
-            //}
-
-            //try
-            //{//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Изменение
-            // //db.НовыйСамолет(Convert.ToInt32(КодСамолета.Text), ((Авиакомпании)Наименование.SelectedValue).КодАвиакомпании, МодельСамолета.Text, Convert.ToInt32(КоличествоМест.Text));
-            //    #region СозданиеНовогоБилета
-            //    p1 = new Билеты();
-            //    p1.КодБилета = Convert.ToInt64(КодБилета.Text);
-            //    #region Проверка наличия такого рейса и его присваивание
-            //    int RaceId = Convert.ToInt32(КодРейса.Text);
-            //    var codes = from table in db.Рейсы
-            //                where table.КодРейса == RaceId
-            //                select table;
-            //    if (codes.Count() == 0) throw new Exception("Не существует указанного рейса");
-            //    else p1.КодРейса = RaceId;
-            //    #endregion
-            //    p1.КодАвиакомпании = db.Авиакомпании.Local.ToBindingList().Where(p => p.Наименование == Наименование.Text).First().КодАвиакомпании;
-            //    p1.НазваниеКласса = НазваниеКласса.Text;
-            //    #region Багаж
-            //    if (Багаж.Text == "Есть") p1.Багаж = true;
-            //    else p1.Багаж = false;
-            //    #endregion
-            //    #region Получение кода пассажира
-            //    var passengers = db.Пассажиры.Local.ToBindingList().
-            //        Where(p => p.Фамилия == Фамилия.Text).
-            //        Where(p => p.Имя == Имя.Text).
-            //        Where(p => p.Отчество == Отчество.Text);
-            //    if (passengers.Count() == 0) throw new Exception("Не существует человека с таким ФИО!");
-            //    else
-            //    {
-            //        int passengerId = passengers.First().КодПассажира;
-            //        var tickets = from table in db.Билеты
-            //                    where table.КодПассажира == passengerId
-            //                    select table;
-            //        if (tickets.Count() != 0) throw new Exception("Данный пассажир уже имеет билет!");
-            //        else p1.КодПассажира = passengerId;
-            //    }
-            //    #endregion
-            //    p1.ДатаПокупкиБилета = DateTime.Now.Date;
-            //    p1.ВремяПокупкиБилета = DateTime.Now.TimeOfDay;
-            //    #endregion
-            //    db.Рейсы.Add(p1);
-            //    //Сохраняем изменения
-            //    db.SaveChanges();
-            //    Close();
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message.ToString());
-            //}
+            try
+            {//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Изменение
+             //db.НовыйСамолет(Convert.ToInt32(КодСамолета.Text), ((Авиакомпании)Наименование.SelectedValue).КодАвиакомпании, МодельСамолета.Text, Convert.ToInt32(КоличествоМест.Text));
+                #region СозданиеНовогоРейса
+                p1 = new Рейсы();
+                #region Проверка наличия такого рейса и его присваивание
+                int RaceId = Convert.ToInt32(КодРейса.Text);
+                var codes = from table in db.Рейсы
+                            where table.КодРейса == RaceId
+                            select table;
+                if (codes.Count() != 0) throw new Exception("Существует указанный рейс");
+                else p1.КодРейса = RaceId;
+                #endregion
+                p1.КодАвиакомпании = db.Авиакомпании.Local.ToBindingList().Where(p => p.Наименование == Наименование.Text).First().КодАвиакомпании;
+                p1.КодСамолёта = db.Самолеты.Local.ToBindingList().Where(p => p.МодельСамолета == ((Самолеты)МодельСамолета.SelectedValue).МодельСамолета).КодСамолета;
+                p1.ДатаОтправления = (DateTime)ДатаОтправления.SelectedDate;
+                p1.ДатаПрибытия = (DateTime)ДатаПрибытия.SelectedDate;
+                p1.ВремяОтправления = new TimeSpan(Convert.ToInt32(Часы.Text), Convert.ToInt32(Минуты.Text), 0);
+                p1.ВремяПрибытия = new TimeSpan(Convert.ToInt32(Часы1.Text), Convert.ToInt32(Минуты1.Text), 0);
+                p1.ДлительностьПерелета = ДлительностьПерелета.Text; 
+                p1.СтоимостьБилета = Convert.ToDecimal(СтоимостьБилета.Text);
+                p1.Отправление = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_отправление.SelectedValue).Город);
+                p1.Прибытие = db.Аэропорты.Local.ToBindingList().Where(p => p.Город == ((Аэропорты)Город_назначение.SelectedValue).Город);
+                #endregion
+                db.Рейсы.Add(p1);
+                //Сохраняем изменения
+                db.SaveChanges();
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
 
         }
 
@@ -110,22 +101,10 @@ namespace Tickets
             db.Самолеты.Load();
             //Поличаем список из другой таблицы
             МодельСамолета.ItemsSource = db.Самолеты.Local.ToBindingList();
-          
+            Наименование.ItemsSource = db.Авиакомпании.Local.ToBindingList();
+            Город_отправление.ItemsSource = db.Аэропорты.Local.ToBindingList();
+            Город_назначение.ItemsSource = db.Аэропорты.Local.ToBindingList();
         }
-
-        //private void КодРейса_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    try
-        //    {
-        //        AddTable.ItemsSource = db.Самолеты.Local.ToBindingList().Where(p => p.КодСамолета == ((Рейсы)КодРейса.SelectedValue).КодРейса);
-        //        Наименование.Text = db.Авиакомпании.Local.ToBindingList().Where(p => p.КодАвиакомпании == ((Рейсы)КодРейса.SelectedValue).КодАвиакомпании).First().Наименование;
-        //    }
-        //    catch (Exception ex)//Если что пойдет не так - при точке останова глянуть значение ex
-        //    {
-
-        //    }
-        //}
-    
 
         private void МодельСамолета_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -137,16 +116,6 @@ namespace Tickets
             {
 
             }
-        }
-
-        private void Город_отправление_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void Город_назначение_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
